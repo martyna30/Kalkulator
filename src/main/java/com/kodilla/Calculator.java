16eddf444c29aad035838d00729d825568b539e7
martyna30 <martynajudo@gmail.com>
2019-12-04 22:54:45 +0100

    project calculator

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public void add() { }
+
+    public void subtract() {}
+
+    public static void main (String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add();
+        System.out.println("It works");
+    }
+}
