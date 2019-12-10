22424cb5c44c4b0f4d4cc8b65ff7a73ab274391f
martyna30 <prawdzikmartyna@op.pl>
2019-12-11 00:40:51 +0100

    First unit test

---
+++ src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.user;
+
+import sun.java2d.pipe.SpanShapeRenderer;
+
+public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
