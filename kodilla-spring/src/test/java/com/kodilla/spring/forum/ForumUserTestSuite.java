7014b2ec26f354c510d294a4820fa26d3c81a838
martyna30 <prawdzikmartyna@op.pl>
2020-02-26 11:13:13 +0100

    ForumUser Bean

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,4 +1,23 @@
 package com.kodilla.spring.forum;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
 public class ForumUserTestSuite {
+    @Test
+    public void testGetUsername() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        //When
+        ForumUser forumUser = context.getBean(ForumUser.class);
+
+        String name = forumUser.getUsername();
+                //Then
+        Assert.assertEquals("John Smith", name);
+    }
 }
