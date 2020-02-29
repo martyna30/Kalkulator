0368036703abbf830828b84acadd8deb8f81d6aa
martyna30 <prawdzikmartyna@op.pl>
2020-03-01 00:22:18 +0100

    Board

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,4 +1,31 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 public class BoardTestSuite {
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        Board board = context.getBean(Board.class);//new Board
+
+        board.getToDoList().addTask("Task toDo1");
+        board.getInProgressList().addTask("Task inProgress1");
+        board.getDoneList().addTask("Task done1");
+        //When
+        String taskToDo = board.getToDoList().getTasks().get(0);
+        String taskInProgress = board.getInProgressList().getTasks().get(0);
+        String taskDone = board.getDoneList().getTasks().get(0);
+        //Then
+        Assert.assertEquals("Task toDo1", taskToDo);
+        Assert.assertEquals("Task inProgress1", taskInProgress);
+        Assert.assertEquals("Task done1", taskDone);
+    }
 }
+
