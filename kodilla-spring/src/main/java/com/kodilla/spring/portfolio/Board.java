b40678e6257227469814dedcfefd2d23fe852d98
martyna30 <prawdzikmartyna@op.pl>
2020-03-06 01:15:12 +0100

    Board

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Board {
+
     private TaskList toDoList;
 
     private TaskList inProgressList;
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,20 +1,13 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
-    @Autowired
-    private TaskList toDoList;
-    @Autowired
-    private TaskList inProgressList;
-    @Autowired
-    private TaskList doneList;
     @Bean
     public Board getBoardList() {
-        return new Board(toDoList, inProgressList, doneList);
+        return new Board(getToDo(), getInProgress(), getDone());
     }
     @Bean(name = "toDoList")
         public TaskList getToDo() {
