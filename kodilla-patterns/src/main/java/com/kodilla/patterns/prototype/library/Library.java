4e1f03bf27d632233e634218c24467d4a28201ef
martyna30 <prawdzikmartyna@op.pl>
2020-03-06 01:48:36 +0100

    Prototype Library

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -1,4 +1,49 @@
 package com.kodilla.patterns.prototype.library;
 
-public class Library {
+import com.kodilla.patterns.prototype.Board;
+import com.kodilla.patterns.prototype.TasksList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Library extends Prototype {
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public String toString() {
+        String l = "Library [" + name + "]\n";
+        for (Book b : books) {
+            l = l + b.toString() + "\n";
+        }
+        return l;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library) super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for(Book book: books) {
+            clonedLibrary.getBooks().add(book);
+        }
+        return clonedLibrary;
+    }
 }
