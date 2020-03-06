090196f849841d0d6d2636184a7f578c35c4ecdc
martyna30 <prawdzikmartyna@op.pl>
2020-03-06 01:38:23 +0100

    Singleton

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,4 +1,19 @@
 package com.kodilla.patterns.singleton;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class LoggerTestSuite {
+    @Test
+    public void getLastLog() {
+        //Given
+        //When
+        Logger.getInstance().log("Logging has been accomplished");
+        String lastLog = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("Logging has been accomplished", lastLog);
+    }
 }
+
+
+
--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/SettingsFileEngineTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/SettingsFileEngineTestSuite.java
@@ -1,4 +1,53 @@
 package com.kodilla.patterns.singleton;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class SettingsFileEngineTestSuite {
+    //private static SettingsFileEngine settingsFileEngine;
+
+    @BeforeClass
+    public static void openSettingsFile() {
+        //settingsFileEngine = new SettingsFileEngine();
+        //settingsFileEngine.open("myapp.settings");
+        SettingsFileEngine.getInstance().open("myapp.settings");
+    }
+
+    @AfterClass
+    public static void closeSettingsFile() {
+        //settingsFileEngine.close();
+        SettingsFileEngine.getInstance().close();
+    }
+
+    @Test
+    public void testGetFileName() {
+        //Given
+        //When
+        String fileName = SettingsFileEngine.getInstance().getFileName();
+        System.out.println("Opened: " + fileName);
+        //Then
+        Assert.assertEquals("myapp.settings", fileName);
+    }
+
+    @Test
+    public void testLoadSettings() {
+        //Given
+        //When
+        boolean result = SettingsFileEngine.getInstance().loadSettings();
+        //Then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testSaveSettings() {
+        //Given
+        //When
+        boolean result = SettingsFileEngine.getInstance().saveSettings();
+        //Then
+        Assert.assertTrue(result);
+    }
 }
+
+
