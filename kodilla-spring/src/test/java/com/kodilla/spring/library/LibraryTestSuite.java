79a4164eda0cc84bb70bc8c8dd0e949a8f2936a4
martyna30 <prawdzikmartyna@op.pl>
2020-03-06 01:43:49 +0100

    Library

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,4 +1,43 @@
 package com.kodilla.spring.library;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LibraryTestSuite {
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        //When
+        Library library = context.getBean(Library.class);
+        library.loadFromDb();
+        //Then
+    }
+    @Test
+    public void testSaveToDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.saveToDb();
+    }
+
+    public void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(LibraryConfig.class); //na podstawie klasy konfiguracyjnej
+        //When && Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< === Beans list === ");
+    }
 }
