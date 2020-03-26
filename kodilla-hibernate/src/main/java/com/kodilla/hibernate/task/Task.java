7774e9218b8b2307f3c2c13551b34099d0d42af6
martyna30 <prawdzikmartyna@op.pl>
2020-03-26 11:37:59 +0100

    Hibernate

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,4 +1,62 @@
 package com.kodilla.hibernate.task;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity //UTRWALENIE W BAZIE(informacja że obiektu mają być w bazie, a tabele utworzy sam i nazwie sam)
+@Table(name = "tasks") //opcjonale WSKAZANIE nazwy TABELI dla obiektów
 public class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
+
+    public Task() { } //do odtworzenia obiektow z bazy wykorzystywany jest konstruktor bezparametrowy
+
+    public Task (String description, int duration) {
+        this.description = description;
+        this.created = new Date();
+        this.duration = duration;
+    }
+
+    @Id
+    @GeneratedValue  //sam generuje wartości
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column (name="DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @NotNull
+    @Column(name="CREATED")
+    public Date getCreated() {
+        return created;
+    }
+
+    @Column(name="DURATION")
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -1,4 +1,17 @@
 package com.kodilla.hibernate.task.dao;
 
-public interface TaskDao {
+import com.kodilla.hibernate.task.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Transactional
+// Adnotacja @Transactional może być użyta przy konkretnej metodzie lub przy klasie (albo interfejsie) - wówczas oznacza to, że dotyczy ona wszystkich metod udostępnianych przez klasę (lub interfejs).
+// dba o zatwierdzanie i wycofywanie transakcji bazodanowych (COMMIT oraz ROLLBACK wykonywane są przez Springa).
+@Repository//springowa adnotacja ,interfejs powinien byc użyty do utworzenia beana ktory bedzie kontrolerem repozytoriun
+public interface TaskDao extends CrudRepository<Task, Integer> {//klasa ktore obiekty zapisujemy do bazy,oraz typ klucza
+List<Task> findByDuration(int duration);
 }
