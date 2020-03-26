e3e0a529ea620fc68752e269dad8ea2192b8db32
martyna30 <prawdzikmartyna@op.pl>
2020-03-26 22:07:03 +0100

    Encja

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,4 +1,49 @@
 package com.kodilla.hibernate.tasklist;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TASKLIST")
 public class TaskList {
+    private int id;
+    private String listName;
+    private String  desription;
+
+    public TaskList() {}
+
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.desription = description;
+    }
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+    @Column(name = "DESCRIPTION")
+    public String getDesription() {
+        return desription;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public void setDesription(String desription) {
+        this.desription = desription;
+    }
 }
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -1,4 +1,12 @@
 package com.kodilla.hibernate.tasklist.dao;
 
-public class TaskListDao {
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TaskListDao extends CrudRepository<TaskList, Integer> { //kontroler repozytorium -klasy bądź interfejsy, które posiadają określone metody służące
+    // do oddelegowania różnych działań do właściwych miejsc w aplikacji -repozytorum dostarcza nam metody
+    List<TaskList> findByListName(String listName);
 }
+
