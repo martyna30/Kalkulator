84eb232e9a958969cac8083f2da8f0831a30267f
martyna30 <prawdzikmartyna@op.pl>
2020-06-29 21:19:16 +0200

    SecondChallenge

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            String result = secondChallenge.probablyIWillThrowException(1,1.5);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Exception: "+ e.getMessage());
+        } finally {
+            System.out.println("The operation has been completed");
+        }
+    }
+}
